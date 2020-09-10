1b19b6fb8213ae2e88a9719bdcdb94f71cf96bca
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-09-10 13:50:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [inputValue, setInputvalue] = useState('');
+  const [urlArray, setUrlArray] = useState([]);
+
+  const handleChange = (e) => {
+    setInputvalue({ ...inputValue, [e.target.name]: e.target.value });
+    console.log(inputValue);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post('https://rel.ink/api/links/', { url: inputValue.url })
+      .then((data) => {
+        const newUrl = data.data.hashid;
+        setUrlArray([...urlArray, newUrl]);
+        console.log(urlArray);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <div className='app-wrapper'>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              placeholder='Enter URL to Shorten &crarr;'
+              onChange={handleChange}
+              name='url'
+              value={inputValue.url}
+            />
+          </form>
+
+          <div className='url-data'>
+            <div className='url-link'>
+              {urlArray.map((urlShorted) => (
+                <div className='link'>
+                  <a
+                    target='blank'
+                    className='urlname'
+                    href={`https://rel.ink/${urlShorted}`}
+                  >
+                    https://rel.ink/{urlShorted}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
